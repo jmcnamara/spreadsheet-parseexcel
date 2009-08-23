473cb83d8e6b1607b95fa7656c801ea35fcb89b3
John McNamara <jmcnamara@cpan.org>
2009-08-23 11:40:49 +0000

    Removed unused files.
    git-svn-id: http://localhost/svn/spreadsheet-parseexcel/trunk@72 a4cd3b43-ee9b-4c8b-9b85-5697f888d59d

--- sample/warning.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-
-use Spreadsheet::ParseExcel;
-
-my $oBook = Spreadsheet::ParseExcel::Workbook->Parse('sample/Excel/gives-warnings.xls');
-print "A1=" . $oBook->{Worksheet}->[0]->{Cells}[0][0]->Value . "\n";
-print "A2=" . $oBook->{Worksheet}->[0]->{Cells}[1][0]->Value . "\n";
-
