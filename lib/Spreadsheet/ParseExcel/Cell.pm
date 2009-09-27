9b7c87e34a436a06be3b13f592eaf45f0230b495
John McNamara <jmcnamara@cpan.org>
2009-09-28 00:50:27 +0100

    Updated documentation. Ongoing.

--- lib/Spreadsheet/ParseExcel/Cell.pm
+++ lib/Spreadsheet/ParseExcel/Cell.pm
@@ -95,6 +95,19 @@ sub encoding {
 
     my $self = shift;
 
+    if ( !defined $self->{Code} ) {
+        return 1;
+    }
+    elsif ( $self->{Code} eq 'ucs2' ) {
+        return 2;
+    }
+    elsif ( $self->{Code} eq '_native_' ) {
+        return 3;
+    }
+    else {
+        return 0;
+    }
+
     return $self->{Code};
 }
 
@@ -171,7 +184,11 @@ The C<value()> method returns the formatted value of the cell.
 
     my $value = $cell->value();
 
-Returns 0 if the property isn't set.
+Formatted in this sense refers to the numeric fomat of the cell value. For example a number such as 40177 might be formatted as 40,117, 40117.000 or even as the date 2009/12/30.
+
+If the cell doesn't contain a numeric format then the formatted and unformated cell values are the same, see the C<unformatted()> method below.
+
+For a defined C<$cell> the C<value()> method will always return a value. In the case of a cell with formatting but no numeric or string contents the method will return the empyt string C<''>.
 
 
 =head2 unformatted()
@@ -180,25 +197,25 @@ The C<unformatted()> method returns the unformatted value of the cell.
 
     my $unformatted = $cell->unformatted();
 
-Returns 0 if the property isn't set.
+Returns the cell value without a numeric format. See the C<value()> method above.
 
 
 =head2 get_format()
 
-The C<get_format()> method returns the L<"Format"> object for the cell.
+The C<get_format()> method returns the L<Spreadsheet::ParseExcel::Format> object for the cell.
 
     my $format = $cell->get_format();
 
-Returns 0 if the property isn't set.
+If a user defined format hasn't been applied to the cell then the default cell format is returned.
 
 
 =head2 type()
 
-The C<type()> method returns the type of cell such as Text, Numeric or Date. If the type was detected as Numeric, and the Cell Format matches m{^[dmy][-\\/dmy]*$}, it will be treated as a Date type.
+The C<type()> method returns the type of cell such as Text, Numeric or Date. If the type was detected as Numeric, and the Cell Format matches m{^[dmy][-\\/dmy]*$}i, it will be treated as a Date type.
 
     my $type = $cell->type();
 
-Returns 0 if the property isn't set.
+See also L<Dates and Time in Excel>.
 
 
 =head2 encoding()
@@ -216,7 +233,7 @@ The C<is_merged()> method returns true if the cell is merged.
 
     my $is_merged = $cell->is_merged();
 
-Returns 0 if the property isn't set.
+Returns C<undef> if the property isn't set.
 
 
 =head2 get_rich_text()
@@ -228,10 +245,26 @@ The C<get_rich_text()> method returns an array ref of font information about eac
 Returns 0 if the property isn't set.
 
 
+=head1 Dates and Time in Excel
+
+Dates and times in Excel are represented by real numbers, for example "Jan 1 2001 12:30 PM" is represented by the number 36892.521.
+
+The integer part of the number stores the number of days since the epoch and the fractional part stores the percentage of the day.
+
+A date or time in Excel is just like any other number. To have the number display as a date you must apply an Excel number format to it. Here are some examples.
+
+    Number     Format                      Result
+    39506.5    'dd/mm/yy'                  28/02/08
+    39506.5    'mm/dd/yy'                  02/28/08
+    39506.5    'd-m-yyyy'                  28-2-2008
+    39506.5    'dd/mm/yy hh:mm'            28/02/08 12:00
+    39506.5    'd mmm yyyy'                28 Feb 2008
+    39506.5    'mmm d yyyy hh:mm AM/PM'    Feb 28 2008 12:00 PM
 
 
+TODO ParseExcel date handling functions.
 
-
+For date conversions using the CPAN C<DateTime> framework see L<DateTime::Format::Excel> http://search.cpan.org/search?dist=DateTime-Format-Excel
 
 
 =head1 AUTHOR
